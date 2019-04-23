231d58943eee7dbdb61982fd2cb34d3f20beab41
nardiyansah <janatri1398@gmail.com>
2019-04-23 17:51:31 +0700

    menambah fungsi untuk halaman detail

--- phpmvc/app/controllers/Bakat.php
+++ phpmvc/app/controllers/Bakat.php
@@ -10,5 +10,14 @@ class Bakat extends Controller
         $this->view('bakat/index',$data);
         $this->view('templates/footer');
     }
+
+    public function detail($nomor)
+    {
+        $data['judul'] = 'Detail Programmer';
+        $data['bakat'] = $this->model('Data_Bakat')->getBakatByNomor($nomor);
+        $this->view('templates/header',$data);
+        $this->view('bakat/detail',$data);
+        $this->view('templates/footer');
+    }
 }
 ?>
