71c6efecd6ecfc3d4f3b1e28b564ee231f073497
nardiyansah <janatri1398@gmail.com>
2019-04-24 20:19:07 +0700

    menambah tombol untuk insert data

--- phpmvc/app/views/bakat/index.php
+++ phpmvc/app/views/bakat/index.php
@@ -1,17 +1,47 @@
-
-<table class="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">No</th>
-      <th scope="col">Bakat</th>
-    </tr>
-  </thead>
-  <tbody>
+<div class="container mt-3">
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#formModal">
+  Insert Data Programmer
+</button>
+  <ul class="list-group">
+    <li class="list-group-item list-group-item-dark">Data Programmer</li>
     <?php foreach ($data['bakat'] as $bkt) : ?>
-        <tr>
-            <th scope="row"><?= $bkt['nomor'] ?></th>
-            <td><?= $bkt['bakat'] ?></td>
-        </tr>
+      <li class="list-group-item d-flex justify-content-between align-items-center"><?= $bkt['nama'] ?><a href="<?= BASEURL; ?>/bakat/detail/<?= $bkt['nomor']; ?>" class="badge badge-pill badge-primary">Detail</a></li>
     <?php endforeach; ?>
-  </tbody>
-</table>
+  </ul>
+</div>
+
+<!-- Modal -->
+
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulLabel">Input Data Programmer</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL;?>/bakat/tambah" method="post">
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Nardiyansah">
+          </div>
+          <div class="form-group">
+            <label for="bakat">Keahlian</label>
+            <input type="text" class="form-control" id="bakat" name="bakat" placeholder="Membuat website">
+          </div>
+          <div class="form-group">
+            <label for="email">E-mail</label>
+            <input type="text" class="form-control" id="email" name="email" placeholder="example@mail.com">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
